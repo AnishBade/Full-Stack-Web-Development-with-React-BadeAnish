65374141576d3e019596c6765d71826354a62244
AnishBade <badeanish6045@gmail.com>
2020-07-17 13:57:07 +0545

    Presentational and Container components

--- Front-End Web Development with React/REACT/confusion/src/components/dishDetailComponent.js
+++ Front-End Web Development with React/REACT/confusion/src/components/dishDetailComponent.js
@@ -2,32 +2,54 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class DishdetailComponent extends Component {
+
+    constructor(props) {
+    super(props);
+    this.state = {
+    
+    };
+    }
   render() {
-    const commentList = this.props.dish.comments.map(item => (
-        <li>
-          {item.comment}
-          <br/><br/>
-          -- {item.author},  {item.date}
-          <br/><br/>
-        </li>
-      )
-    );
+    if (this.props.dish) {
+        const commentList = this.props.dish.comments.map((comment, i) => (
+          	  <li key={i}>
+                {i}.
+               {comment.comment}
+                <br/><br/>	      
+          -- {comment.author},{" "}
+          {new Intl.DateTimeFormat("en-US", {
+            year: "numeric",
+            month: "short",
+            day: "2-digit"
+          }).format(new Date(Date.parse(comment.date)))}
+          <br />
+          <br />
+        </li>	   
+    ));
+
     return (
-      <div className="row">
-        <div className="col-12 col-md-5 m-1">
-          <Card>
-            <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
-            <CardBody>
-              <CardTitle>{this.props.dish.name}</CardTitle>
-              <CardText>{this.props.dish.description}</CardText>
-            </CardBody>
-          </Card>
-        </div>
-        <div className="col-12 col-md-5 m-1">
-          <ul>{commentList}</ul>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            <Card>
+              <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
+              <CardBody>
+                <CardTitle>{this.props.dish.name}</CardTitle>
+                <CardText>{this.props.dish.description}</CardText>
+              </CardBody>
+            </Card>
+          </div>
+          <div className="col-12 col-md-5 m-1">
+            <ul>{commentList}</ul>
+          </div>
         </div>
       </div>
+
     );
+    }
+    else{
+      return <div></div>
+    }
   }
 }
 
