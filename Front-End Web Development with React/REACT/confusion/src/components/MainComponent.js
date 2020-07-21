3ccdfa8d168ec6d3c9407df875e0c8c6bd4ea0c9
AnishBade <badeanish6045@gmail.com>
2020-07-22 01:39:35 +0545

    Single Page Application

--- Front-End Web Development with React/REACT/confusion/src/components/MainComponent.js
+++ Front-End Web Development with React/REACT/confusion/src/components/MainComponent.js
@@ -1,9 +1,14 @@
 import React,{ Component } from 'react';
 import Home from './HomeComponent';  
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 import DishdetailComponent from './dishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import {COMMENTS} from '../shared/comments';
+import {PROMOTIONS} from '../shared/promotion';
+import {LEADERS} from '../shared/leaders';
+
 import {DISHES} from '../shared/dishes';
 import {Switch, Route,Redirect } from 'react-router-dom';
 
@@ -14,6 +19,9 @@ class Main extends Component {
 
     this.state={
       dishes:DISHES,
+      promotions:PROMOTIONS,
+      leaders:LEADERS,
+      comments:COMMENTS
     };
   }
 
@@ -21,7 +29,16 @@ class Main extends Component {
 
     const HomePage = ()=>{
       return(
-        <Home/>
+        <Home dish={this.state.dishes.filter((dish)=>dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo)=>promo.featured)[0]} 
+          leader={this.state.leaders.filter((leader)=>leader.featured)[0]}/>
+      );
+    }
+
+    const DishWithId=({match})=>{
+      return(
+        <DishdetailComponent dish={this.state.dishes.filter((dish)=>dish.id=== parseInt(match.params.dishId,10))[0]} 
+        comments={this.state.comments.filter((comment)=>comment.dishId ===parseInt(match.params.dishId,10))} />
       );
     }
     return (
@@ -30,6 +47,8 @@ class Main extends Component {
         <Switch>
           <Route path='/home' component={HomePage} />
           <Route exact path='/menu' component={()=><Menu dishes={this.state.dishes}/>} />
+          <Route path='/menu/:dishId' component={DishWithId}/>
+          <Route exact path='/contactus'component={Contact}/>
           <Redirect to='/home' />
         </Switch>
         <Footer />
--- Front-End Web Development with React/REACT/confusion/src/components/dishDetailComponent.js
+++ Front-End Web Development with React/REACT/confusion/src/components/dishDetailComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle ,BreadcrumbItem,Breadcrumb} from 'reactstrap';
+import {Link} from 'react-router-dom';
 
   const RenderComment=({comments})=>{
     if (comments !== null || comments.length === 0){
@@ -57,9 +57,26 @@ const DishdetailComponent=(props)=>{
     return (
       <div className="container">
         <div className="row">
+            <Breadcrumb>
+                <BreadcrumbItem>
+                    <Link to="/home">Home</Link>   
+                </BreadcrumbItem>
+                <BreadcrumbItem>
+                    <Link to="/menu">Menu</Link>   
+                </BreadcrumbItem>               
+                <BreadcrumbItem active>
+                    {props.dish.name}
+                </BreadcrumbItem>
+            </Breadcrumb>
+            <div className="col-12">
+                <h3>{props.dish.name} </h3>
+                <hr />
+            </div>
+        </div>
+        <div className="row">
           
           <RenderDish dish={props.dish}/>
-          <RenderComment comments={props.dish.comments}/>
+          <RenderComment comments={props.comments}/>
         </div>
       </div>
 
