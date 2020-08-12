b6216bad06ab18704699abd661d8152f3f7efe7c
AnishBade <badeanish6045@gmail.com>
2020-08-12 12:10:18 +0545

    Redux-Thunk

--- Front-End Web Development with React/REACT/confusion/src/components/dishDetailComponent.js
+++ Front-End Web Development with React/REACT/confusion/src/components/dishDetailComponent.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle ,BreadcrumbItem,Breadcrumb } from 'reactstrap';
 import {Link} from 'react-router-dom';
 import CommentForm from './CommentFormComponent';
-
+import {Loading} from './LoadingComponent';
   const RenderComment=({comments,addComment,dishId})=>{
     if (comments !== null || comments.length === 0){
           const commentList = comments.map((comment, i) => {
@@ -59,7 +59,26 @@ import CommentForm from './CommentFormComponent';
  
 
 const DishdetailComponent=(props)=>{
-  if(props.dish){
+  
+  if(props.isLoading){
+    return(
+      <div className="container">
+        <div className="row">
+          <Loading/>
+        </div>
+      </div>
+    );
+  }
+  else if(props.errMess){
+    return(
+      <div className="container">
+        <div className="row">
+          <h4>{props.errMess}</h4>
+        </div>
+      </div>
+    )
+  }
+  else if(props.dish!=null){
     return (
       <div className="container">
         <div className="row">
